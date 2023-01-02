81ca4551c6f4a2e5e114747d0c7a93923336315e
Marc Brooks <IDisposable@gmail.com>
2023-01-02 13:03:14 -0600

    Fix bitrot
    As with many things from the 90s, the source paper has withered away... found a reference to the PDF, mined for the Archive.org link.

--- DynamicSample/Gender.cs
+++ DynamicSample/Gender.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace DynamicComparerSample
 {
-    public enum Gender // see http://www.mtsu.edu/~phollowa/5sexes.html
+    public enum Gender // see https://web.archive.org/web/20220622015733/https://danielwharris.com/teaching/360/readings/FaustoSterling.pdf
+                       // was originally at http://www.mtsu.edu/~phollowa/5sexes.html
     {
         Male = 0,
         Female = 1,
